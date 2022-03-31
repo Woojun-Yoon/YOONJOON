842159ee8b3e930f237e1145ee098a169d15b593
Woojun-Yoon <dbsdnwns11@naver.com>
2022-03-31 22:44:24 +0900

    commit one

--- 0000 PRACTICE.py
+++ 0000 PRACTICE.py
@@ -1,8 +1,14 @@
+# if you know, you can do everything
 from sys import stdin
 from collections import deque
 from itertools import permutations
+from itertools import combinations
 from collections import Counter
 from bisect import bisect_left, bisect_right
+from collections import defaultdict
+from math import comb
+from heapq import heapify
+
 input = stdin.readline
 
 n, *data = map(int, input().split()) # split()은 공백, 엔터, 탭으로 토큰화 default
