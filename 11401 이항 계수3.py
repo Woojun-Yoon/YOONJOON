b9f0d810c306f5246a67866a10a18aaa0f01f950
Woojun-Yoon <dbsdnwns11@naver.com>
2022-04-01 15:26:12 +0900

    commit one

--- 11401 이항 계수3.py
+++ 11401 이항 계수3.py
@@ -1,7 +1,8 @@
 from sys import stdin
+from sys import exit
 input = stdin.readline
 limit = 1000000007
-
+'''
 n, k = map(int, input().split())
 if n - k < k:
     k = n - k
@@ -17,3 +18,27 @@ for i in range(1, k):
     bottom = bottom * (i + 1) % limit
 
 print(top * pow(bottom, -1, limit) % limit)
+'''
+# 분할정복 거듭제곱 사용 1.1s 
+def power(a, b):
+    result = 1
+    while(b > 0):
+        if (b % 2) == 1:
+            result = (result * a) % limit
+        a = (a * a) % limit
+        b = b // 2
+    return result
+# DP로 시간 단축? (int 범위 초과로 메모리 공간 부족)
+def fac(n):
+    result = 1
+    for i in range(2, n + 1):
+        result = (result * i) % limit
+    return result
+
+n, k = map(int, input().split())
+if (k == 0) or (n == k):
+    print(1)
+    exit()
+
+a, b, c = fac(n), fac(k), fac(n - k)
+print(a * power(b * c, limit - 2) % limit)
