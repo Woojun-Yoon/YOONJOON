47688361774be9af7bf8336f5c0afb8c4d1b00a5
Woojun-Yoon <dbsdnwns11@naver.com>
2022-05-03 15:04:12 +0900

    commit one

---
+++ 2748 피보나치 수2.py
@@ -0,0 +1,20 @@
+from sys import stdin
+input = stdin.readline
+
+dic = [0] * 100
+dic[1] = 1
+
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif dic[n] == 0:
+        dic[n] = fibo(n-1) + fibo(n-2)
+        return dic[n]
+    else:
+        return dic[n]
+
+n = int(input()) #테스트 개수 T
+fibo(n)
+print(dic[n])
