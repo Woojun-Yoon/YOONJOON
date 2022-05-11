2b9fd02da503705925320701fa5a3158e1b987b7
Woojun-Yoon <dbsdnwns11@naver.com>
2022-05-11 19:22:12 +0900

    commit one

--- 15649 N과 M(1).py
+++ 15649 N과 M(1).py
@@ -1,9 +1,24 @@
 from sys import stdin
 from itertools import permutations
 input = stdin.readline
-
+'''
 n, m = map(int, input().split())
 nums = [i for i in range(1, n + 1)]
 # join and map으로 60ms 시간 단축 가능
 for cache in permutations(nums, m):
     print(*cache) # unpacking 보다 join이 더 빠름
+'''
+n, m = map(int, input().split())
+nums = []
+
+def back():
+    if len(nums) == m:
+        print(' '.join(map(str, nums)))
+        return
+    
+    for i in range(1, n + 1):
+        nums.append(i)
+        back()
+        nums.pop()
+
+back()
